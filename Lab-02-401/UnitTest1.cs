a742e40cf0dea6ed6644e5c3ccd65f7a47d2f2d0
ChristianLievrouw <celievrouw1@gmail.com>
2020-08-25 22:18:43 -0500

    finished lab

--- Lab-02-401/UnitTest1.cs
+++ Lab-02-401/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using UnitTesting;
 using Xunit;
 
 namespace Lab_02_401
@@ -6,9 +7,41 @@ namespace Lab_02_401
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void ViewBalance_displays_current_balance()
         {
+            //Arange
+            decimal balance = 100.00m;
+            //Act
+            decimal result = Program.ViewBalance(balance);
+            //Assert
+            decimal expected = balance;
+            Assert.Equal(expected, result);
+        }
 
+        [Fact]
+        public void Withdraw_subracts_number_from_balance()
+        {
+            //Arange
+            decimal balance = 100.00m;
+            decimal widthdraw = 25.00m;
+            //Act
+            decimal result = Program.Withdraw(balance, widthdraw);
+            //Assert
+            decimal expected = 75.00m;
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Deposit_input_number_from_user_to_balance()
+        {
+            //Arange
+            decimal balance = 100.00m;
+            decimal deposit = 50.00m;
+            //Act
+            decimal result = Program.Deposit(balance, deposit);
+            //Assert
+            decimal expected = 150.00m;
+            Assert.Equal(expected, result);
         }
     }
 }
